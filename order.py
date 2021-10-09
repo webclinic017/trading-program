a247865add7591bb1d5a8905d1c2dcf0e3eaf372
Louis Chan <louis971223@gmail.com>
2021-10-10 01:53:10 +0800

    make it better

--- order.py
+++ order.py
@@ -5,16 +5,16 @@ import datetime as dt
 class Order:
     def __init__(self):
         self.client = Connect().make_connection()
-        self.client.futures_change_leverage(symbol='BTCUSDT', leverage=50)
+        self.client.futures_change_leverage(symbol='ETHUSDT', leverage=50)
         balance = float(self.client.futures_account_balance()[1]['balance'])
         eth_last_price = float(self.client.futures_recent_trades(
-                    symbol='BTCUSDT')[-1]['price'])
+                    symbol='ETHUSDT')[-1]['price'])
         self.my_quantity = round(float(balance*50/eth_last_price*0.3),2)
 
     def sell(self, last_price):
 
         order = self.client.futures_create_order(
-        symbol='BTCUSDT',
+        symbol='ETHUSDT',
         type=self.client.ORDER_TYPE_MARKET,
         side=self.client.SIDE_SELL,
         quantity= self.my_quantity,
@@ -27,7 +27,7 @@ class Order:
     def buy(self, last_price):
 
         order = self.client.futures_create_order(
-        symbol='BTCUSDT',
+        symbol='ETHUSDT',
         type=self.client.ORDER_TYPE_MARKET,
         side=self.client.SIDE_BUY,
         quantity= self.my_quantity,
@@ -40,7 +40,7 @@ class Order:
 
         if side == "BUY":
             order = self.client.futures_create_order(
-            symbol='BTCUSDT',
+            symbol='ETHUSDT',
             side=self.client.SIDE_SELL,
             type=self.client.ORDER_TYPE_MARKET,
             quantity=qty,
@@ -49,7 +49,7 @@ class Order:
             )
         elif side == "SELL":
             order = self.client.futures_create_order(
-            symbol='BTCUSDT',
+            symbol='ETHUSDT',
             side=self.client.SIDE_BUY,
             type=self.client.ORDER_TYPE_MARKET,
             quantity=qty,
