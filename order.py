8b9605f7b6758a762ffa297ee89b2d336c6247a4
Louis Chan <louis971223@gmail.com>
2021-10-18 04:13:03 +0800

    make it better

--- order.py
+++ order.py
@@ -26,7 +26,7 @@ class Order:
         stochrsi = (close_rsi - min_rsi)/(max_rsi - min_rsi)
         k = talib.SMA(stochrsi, 3)*100
         d = talib.SMA(k, 3)
-        quantity = 0.2
+        quantity = 0.18
         if k[-1]<d[-1]:
             buy_quantity = quantity*0.65
             sell_quantity = quantity
